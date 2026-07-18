4a0dc577392cc1fec882e753489cc21294d4528a
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-18 16:11:16 +0545

    Initialized the project

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
