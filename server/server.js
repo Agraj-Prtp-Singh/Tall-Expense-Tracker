f7ec1df05a0999a8689cdc3bc17ba19a36cb6031
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-19 17:47:26 +0545

    Connected POST in the server

--- server/server.js
+++ server/server.js
@@ -3,15 +3,31 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+const expenseRoutes = require("./routes/expenseRoutes");
+
 const app = express();
 
+// Middleware
+
+app.use(express.json());
+
+// Routes
+app.use("/api/expenses", expenseRoutes);
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// MongoDB connection
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server Running On Port ${PORT}🚀.`);
+    });
+  })
+
+  .catch((err) => {
+    console.error("MongoDB connection Failed.");
+    console.error(err);
+  });
