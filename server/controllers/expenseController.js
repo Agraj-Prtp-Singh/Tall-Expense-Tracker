d0b55057c26228b08eeec642409e9865f474b80b
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-19 17:46:13 +0545

    Completed POST

---
+++ server/controllers/expenseController.js
@@ -0,0 +1,26 @@
+const Expense = require("../models/Expense");
+
+const createExpense = async (req, res) => {
+  try {
+    const { title, amount, category, paymentMethod, date, note } = req.body;
+
+    const expense = await Expense.create({
+      title,
+      amount,
+      category,
+      paymentMethod,
+      date,
+      note,
+    });
+
+    res.status(201).json(expense);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+
+
+module.exports = { createExpense };
