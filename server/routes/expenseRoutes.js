3ed2217e290d1f569a11f8731a78e34e02343b13
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-23 19:45:41 +0545

    Added routes for PUT Method

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const {
   createExpense,
   getExpenses,
+  updateExpenses,
 } = require("../controllers/expenseController");
 
 router.post("/", createExpense);
 router.get("/", getExpenses);
+router.put("/:id", updateExpenses);
 module.exports = router;
