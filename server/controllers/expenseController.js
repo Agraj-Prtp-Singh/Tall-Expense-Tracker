2092faed9a71116b086bb9e35ff8bf60c62f7da8
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-23 19:45:13 +0545

    Created PUT Method

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -34,4 +34,28 @@ const getExpenses = async (req, res) => {
   }
 };
 
-module.exports = { createExpense, getExpenses };
+// Put Method
+
+const updateExpenses = async (req, res) => {
+  try {
+    const updatedExpense = await Expense.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    if (!updateExpenses) {
+      return res.status(404).json({
+        message: "Expense not found",
+      });
+    }
+    res.status(200).json(updateExpenses);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+module.exports = { createExpense, getExpenses, updateExpenses };
