57228f2509bed088359e92a28fc29c572f5b9347
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-25 23:41:00 +0545

    Implement JWT authentication and secure expense ownership

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -1,6 +1,6 @@
 const Expense = require("../models/Expense");
 
-// POST Method
+// Create Expense
 const createExpense = async (req, res) => {
   try {
     const { title, amount, category, paymentMethod, date, note } = req.body;
@@ -12,6 +12,7 @@ const createExpense = async (req, res) => {
       paymentMethod,
       date,
       note,
+      user: req.user.id,
     });
 
     res.status(201).json(expense);
@@ -22,10 +23,14 @@ const createExpense = async (req, res) => {
   }
 };
 
-// Get Method
+// Get All Expenses
+
 const getExpenses = async (req, res) => {
   try {
-    const expenses = await Expense.find().sort({ date: -1 });
+    const expenses = await Expense.find({
+      user: req.user.id,
+    }).sort({ date: -1 });
+
     res.status(200).json(expenses);
   } catch (error) {
     res.status(500).json({
@@ -34,10 +39,24 @@ const getExpenses = async (req, res) => {
   }
 };
 
-// Put Method
-
+// Update Expense
 const updateExpenses = async (req, res) => {
   try {
+    const expense = await Expense.findById(req.params.id);
+
+    if (!expense) {
+      return res.status(404).json({
+        message: "Expense not found",
+      });
+    }
+
+    // Check ownership
+    if (expense.user.toString() !== req.user.id) {
+      return res.status(403).json({
+        message: "You are not authorized to update this expense.",
+      });
+    }
+
     const updatedExpense = await Expense.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -46,11 +65,7 @@ const updateExpenses = async (req, res) => {
         runValidators: true,
       },
     );
-    if (!updatedExpense) {
-      return res.status(404).json({
-        message: "Expense not found",
-      });
-    }
+
     res.status(200).json(updatedExpense);
   } catch (error) {
     res.status(500).json({
@@ -59,20 +74,29 @@ const updateExpenses = async (req, res) => {
   }
 };
 
-// Delete Method
+// Delete Expense
 
 const deleteExpenses = async (req, res) => {
   try {
-    const deletedExpense = await Expense.findByIdAndDelete(req.params.id);
+    const expense = await Expense.findById(req.params.id);
 
-    if (!deletedExpense) {
+    if (!expense) {
       return res.status(404).json({
-        message: "Expense Not Found",
+        message: "Expense not found",
       });
     }
+
+    // Check ownership
+    if (expense.user.toString() !== req.user.id) {
+      return res.status(403).json({
+        message: "You are not authorized to delete this expense.",
+      });
+    }
+
+    await expense.deleteOne();
+
     res.status(200).json({
-      message: "Expense deleted successfully",
-      deletedExpense,
+      message: "Expense deleted successfully.",
     });
   } catch (error) {
     res.status(500).json({
@@ -80,4 +104,10 @@ const deleteExpenses = async (req, res) => {
     });
   }
 };
-module.exports = { createExpense, getExpenses, updateExpenses, deleteExpenses };
+
+module.exports = {
+  createExpense,
+  getExpenses,
+  updateExpenses,
+  deleteExpenses,
+};
