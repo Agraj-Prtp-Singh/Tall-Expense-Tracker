7b7ad3d73dfb88f3cac2ac43b767c513f209d1ac
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-27 16:06:26 +0545

    Added Register Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import LeftPanel from "./components/auth/LeftPanel";
+import RegisterForm from "./components/auth/RegisterForm";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default App;
