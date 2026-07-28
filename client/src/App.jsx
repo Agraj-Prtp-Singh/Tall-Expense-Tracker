e729037be0ffd56a844028a4ec8c587155f779c3
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-28 18:11:51 +0545

    Initialized Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import LeftPanel from "./components/auth/LeftPanel";
 import RightPanel from "./components/auth/RightPanel";
+import Sidebar from "./components/layout/Sidebar";
 
 const App = () => {
   return (
-    <div className="flex h-screen overflow-hidden">
-      <LeftPanel />
-      <RightPanel />
+    // <div className="flex h-screen overflow-hidden">
+    //   <LeftPanel />
+    //   <RightPanel />
+    // </div>
+    <div>
+      <Sidebar />
     </div>
   );
 };
