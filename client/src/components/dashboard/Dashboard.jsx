6ac37c1008f4339c86f8525139b87a3e2b9a6c3b
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-07-28 21:30:30 +0545

    Added Stats component

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DashboardNav from "./DashboardNav";
+import Stats from "./Stats";
 
 const Dashboard = () => {
   return (
     <div>
       <DashboardNav />
+      <Stats />
     </div>
   );
 };
