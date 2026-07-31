5b480c4b769b2d6213e97d676029835ef7b0ea05
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-01 01:14:12 +0545

    Add SearchBar component

--- client/src/pages/Expense.jsx
+++ client/src/pages/Expense.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ExpenseNav from "../components/expense/ExpenseNav";
+import SearchBar from "../components/expense/SearchBar";
 
 const Expense = () => {
   return (
     <div>
       <ExpenseNav />
+      <SearchBar />
     </div>
   );
 };
