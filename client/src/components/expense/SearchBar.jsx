b73ccb38ec8221298b5b224d9906288e87e1f3f6
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-01 01:01:38 +0545

    Implement Search bar

---
+++ client/src/components/expense/SearchBar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Search } from "lucide-react";
+import { initialExpenses } from "../../data/expenses";
+
+const SearchBar = () => {
+  const [search, setSearch] = useState("");
+
+  const filteredExpenses = initialExpenses.filter((initialExpenses) =>
+    initialExpenses.title.toLowerCase().includes(search.toLowerCase()),
+  );
+  return (
+    <>
+      <div className="flex items-center gap-3 w-full max-w-md border border-gray-200 rounded-xl px-4 py-3 bg-white">
+        <Search size={18} className="text-gray-400" />
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search Expenses..."
+          className="flex-1 outline-none text-sm placeholder:text-gray-400 bg-transparent"
+        />
+      </div>
+      {filteredExpenses.map((initialExpenses) => (
+        <div key={initialExpenses.id}>{initialExpenses.title}</div>
+      ))}
+    </>
+  );
+};
+
+export default SearchBar;
