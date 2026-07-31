1d36e2d1f208e4a7c8a36b9a24abcce4e8e38145
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-01 00:39:56 +0545

    Move data for categories

---
+++ client/src/data/categories.js
@@ -0,0 +1,18 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Groceries",
+  },
+  {
+    id: 2,
+    name: "Transport",
+  },
+  {
+    id: 3,
+    name: "Subscriptions",
+  },
+  {
+    id: 4,
+    name: "Food & Dining",
+  },
+];
