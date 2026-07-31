c61e8a6fd5040ea9c57dd0f87fbda89ca115880c
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-01 01:12:18 +0545

    Update UI of  Search bar

--- client/src/components/expense/SearchBar.jsx
+++ client/src/components/expense/SearchBar.jsx
@@ -8,22 +8,23 @@ const SearchBar = () => {
   const filteredExpenses = initialExpenses.filter((initialExpenses) =>
     initialExpenses.title.toLowerCase().includes(search.toLowerCase()),
   );
+
   return (
-    <>
-      <div className="flex items-center gap-3 w-full max-w-md border border-gray-200 rounded-xl px-4 py-3 bg-white">
+    <div className=" mt-4 pl-7">
+      <div className="flex items-center gap-3 w-full max-w-md border bg-[#F5F9FF] border-gray-200 rounded-xl px-4 py-3">
         <Search size={18} className="text-gray-400" />
         <input
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Search Expenses..."
-          className="flex-1 outline-none text-sm placeholder:text-gray-400 bg-transparent"
+          className="flex-1  outline-none text-sm placeholder:text-gray-400 bg-transparent"
         />
       </div>
       {filteredExpenses.map((initialExpenses) => (
         <div key={initialExpenses.id}>{initialExpenses.title}</div>
       ))}
-    </>
+    </div>
   );
 };
 
