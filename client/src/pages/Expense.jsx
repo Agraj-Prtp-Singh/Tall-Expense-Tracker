a1e6a2054dcdafeed5689cf71b6ab89af9930472
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-01 00:40:24 +0545

    Initialize

---
+++ client/src/pages/Expense.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExpenseNav from "../components/expense/ExpenseNav";
+
+const Expense = () => {
+  return (
+    <div>
+      <ExpenseNav />
+    </div>
+  );
+};
+
+export default Expense;
