75971d30f7fd4a7f7aa755d5cdd3b3f942418d13
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-01 00:37:36 +0545

    Change styling od Header

--- client/src/components/dashboard/DashboardNav.jsx
+++ client/src/components/dashboard/DashboardNav.jsx
@@ -11,14 +11,14 @@ const user = [
 const DashboardNav = () => {
   return (
     <div className=" flex justify-between font-newsreader">
-      {/* Text Part */}
-      <div className="p-8 ">
-        <p className="block text-[30px] ">
+      {/* Header */}
+      <div className="pl-8 pt-2">
+        <h1 className="block text-[30px] font-semibold">
           Good Evening,
           {user.map((person) => (
             <span key={person.email}> {person.name}</span>
           ))}
-        </p>
+        </h1>
         <p className="block text-[15.5px] text-[#75746E]">
           Here's what's happening with your money this{" "}
           {user.map((person) => (
