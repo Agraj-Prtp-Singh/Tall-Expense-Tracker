061a1935ea4eae028dbf34c7d8a20bad5e47c46d
Agraj Pratap Singh <singhagraj021@gmail.com>
2026-08-08 10:16:53 +0545

    Wrap the component in Expense Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,28 @@ import LeftPanel from "./components/auth/LeftPanel";
 import RightPanel from "./components/auth/RightPanel";
 import Sidebar from "./components/layout/Sidebar";
 import Dashboard from "./pages/Dashboard";
-import "react-datepicker/dist/react-datepicker.css";
 import Expense from "./pages/Expense";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 import { Routes, Route } from "react-router-dom";
+import { ExpenseProvider } from "./context/ExpenseContext";
 
 const App = () => {
   return (
-    // <div className="flex h-screen overflow-hidden">
-    //   <LeftPanel />
-    //   <RightPanel />
-    // </div>
-    <div className="flex min-h-screen bg-gray-50">
-      <Sidebar />
+    <ExpenseProvider>
+      <div className="flex min-h-screen">
+        <Sidebar />
 
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/expenses" element={<Expense />} />
-        </Routes>
-      </main>
-    </div>
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+
+            <Route path="/expenses" element={<Expense />} />
+          </Routes>
+        </main>
+      </div>
+    </ExpenseProvider>
   );
 };
 
